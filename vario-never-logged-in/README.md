8df695cc3382e94c71fc4326b4ca3a6ce4aeab04
MMyers-Vario <98182719+MMyers-Vario@users.noreply.github.com>
2022-05-10 10:04:45 -0400

    Add files via upload
    Initial upload

---
+++ vario-never-logged-in/README.md
@@ -0,0 +1,9 @@
+# vario-never-logged-in
+
+Auto-assigns the "Never Logged In" role to new users on account creation and removes this role the first time that they log in.
+
+Installation:
+
+1. Download the .zip of this repo.
+2. Upload as a plugin into Wordpress.
+3. Activate plugin.
---
+++ vario-never-logged-in/plugin.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Vario Never Logged In
+Plugin URI: https://virtualbyvario.com
+Description: Sets a User Role (Never Logged In) for new users, which is removed once the user logs in.
+Version: 1.05
+Author: Virtual by Vario
+Author URI: https://virtualbyvario.com
+*/
+
+
+// Create a new user role
+function nli_new_role() {  
+ 
+    //add the new user role
+    add_role(
+        'never_logged_in',
+        'Never Logged In',
+        array()
+    );
+ 
+}
+add_action('admin_init', 'nli_new_role');
+ 
+
+add_action( 'user_register', function( $user_id ) {
+  $user = get_user_by( 'ID', $user_id );
+    $user->add_role( 'never_logged_in' );
+});
+
+function vario_remove_nli( $user_login, $user ) {
+        //remove the Never Logged In user role if they have it
+		$user->remove_role( 'never_logged_in' );
+}
+add_action('wp_login', 'vario_remove_nli', 10, 2);
+
+?>
